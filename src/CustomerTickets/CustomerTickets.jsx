52d71b4e3ffb7a57520dc6b4bdd5ce209ffc1a87
Joy <njrksb@gmail.com>
2026-03-04 22:22:16 +0600

    added some correction code

--- src/CustomerTickets/CustomerTickets.jsx
+++ src/CustomerTickets/CustomerTickets.jsx
@@ -3,16 +3,23 @@ import CustomerCards from './CustomerCards';
 import Task from './Task';
 
 
-const CustomerTickets = ({customerPromise, customersTask, setCustomersTask}) => {
+const CustomerTickets = ({ customerPromise, customersTask, setCustomersTask, setInProgressCount, inProgressCount, resolvedCount, setResolvedCount }) => {
     return (
         <div className='lg:flex lg:justify-between gap-5 lg:px-10 py-10'>
             <div className='lg:w-9/12'>
                 <h1 className='text-3xl font-bold'>Customer Tickets</h1>
 
                 <div>
-                   <Suspense>
-                     <CustomerCards customerPromise={customerPromise} customersTask={customersTask} setCustomersTask={setCustomersTask}></CustomerCards>
-                   </Suspense>
+                    <Suspense>
+                        <CustomerCards
+                            customerPromise={customerPromise}
+                            customersTask={customersTask} setCustomersTask={setCustomersTask}
+                            setInProgressCount={setInProgressCount}
+                            inProgressCount={inProgressCount}
+                            resolvedCount={resolvedCount}
+                            setResolvedCount={setResolvedCount}
+                        ></CustomerCards>
+                    </Suspense>
                 </div>
             </div>
 
@@ -21,8 +28,12 @@ const CustomerTickets = ({customerPromise, customersTask, setCustomersTask}) => 
             <div className=' lg:w-3/12 px-2 space-y-2'>
                 <h1 className='text-3xl font-semibold'>Task Status</h1>
                 <p>Select a ticket to add to Task Status</p>
+               
 
-                <Task customerPromise={customerPromise} customersTask={customersTask} setCustomersTask={setCustomersTask}></Task>
+                <Task customerPromise={customerPromise} customersTask={customersTask} setCustomersTask={setCustomersTask}
+                resolvedCount={resolvedCount}
+                setResolvedCount={setResolvedCount}
+                ></Task>
 
                 <div className='space-y-2'>
                     <h1 className='text-3xl font-semibold'>Resolved Task</h1>
--- src/CustomerTickets/Task.jsx
+++ src/CustomerTickets/Task.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
-const Task = ({customerPromise, customersTask, setCustomersTask}) => {
+const Task = ({ customersTask, resolvedCount, setResolvedCount, setInProgressCount, inProgressCount}) => {
+
+    const handleResolve = () => {
+        setResolvedCount(resolvedCount + 1)
+        setInProgressCount(inProgressCount - 1)
+        
+    }
     return (
-        <div>
-            ffddffdf
+        <div className='space-y-8 py-5'>
+
+            {
+                customersTask.map(customer =>
+                    <div className="card bg-base-100  shadow-lg">
+                        <div className="card-body ">
+                            <h2 className="text-xl font-semibold">{customer.title}</h2>
+                            <button onClick={()=>{
+                                handleResolve(customer)
+                               
+                                    
+                            }} className="btn bg-[#02A53B] btn-block text-white">Complete</button>
+                        </div>
+                    </div>)
+            }
+
         </div>
     );
 };
 
 export default Task;
+
+
